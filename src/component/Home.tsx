04b34e5fdcdd2e61cdb14b34428e41c73f85c28b
mdalaminfaraji <alaminice1617@gmail.com>
2023-07-25 02:33:18 +0600

    todo list

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InputField from './InputField';
+import { Todo } from './Modal';
+import TodoList from './TodoList';
 
 const Home:React.FC = () => {
+    const [todo, setTodo]=useState<string>("");
+    const [todos, setTodos]=useState<Todo[]>([])
+    const handleAdd=(e:React.FormEvent)=>{
+     e.preventDefault();
+     if(todo){
+        setTodos([...todos, {id:Date.now(),todo, isDone:false}]);
+        setTodo("");
+     }
+    }
     return (
         <div className='App'>
             <h1 className='heading'>Taskify</h1>
+            <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}></InputField>
+            <TodoList todos={todos} setTodos={setTodos}></TodoList>
         </div>
     );
 };
